df7379fb9f66910c3d350d4291bedeac2d6176a5
LucasNithael <lucasnithael0101@gmail.com>
2023-12-17 20:17:32 -0300

    novas definições

--- scripts.js
+++ scripts.js
@@ -24,5 +24,23 @@ $(document).ready(function(){
     $('#add-task svg').click(function(){    
         $('.form').toggle('slow');
     });
+
+
+    $('#open-close img').click(function(){    
+        $('#form-add-task').toggle('slow');
+        var $formAddTask = $('#form-add-task');
+        var $img = $(this);
+
+        // Verifica o atributo src atual
+        if ($img.attr('src') === 'svg/open.svg') {
+            // Se for o SVG 'open.svg', altera para 'close.svg'
+            $img.attr('src', 'svg/close.svg');
+            $formAddTask.slideDown('slow');  // Ou use toggle se preferir
+        } else {
+            // Se for o SVG 'close.svg', altera para 'open.svg'
+            $img.attr('src', 'svg/open.svg');
+            $formAddTask.slideUp('slow');  // Ou use toggle se preferir
+        }
+    })
     
 })
