a38f92e41cde141f7dcb8dfb7b879c48d9162b04
LucasNithael <lucasnithael0101@gmail.com>
2023-12-29 11:49:15 -0300

    feature: fuction to save and delete

--- scripts.js
+++ scripts.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
     // load task local
-    //loadTasks();
+    var sectionsArray = [];
+    loadTasks();
+    //localStorage.clear();
 
     $('#btn-add-task').click(function(){
         text = $('#input-task-text input').val();
@@ -21,11 +23,13 @@ $(document).ready(function(){
         } 
         else {
             makeTask(text);
+            saveTasks();
         }
 
     })
     
     function makeTask(text){
+        debugger;
         time = new Date();
         // quero o dia e a hora
         time = time.toLocaleString();
@@ -34,11 +38,43 @@ $(document).ready(function(){
         <section class="task-section">
             <p class="text-task">${text}</p>
             <div class="time-task">${time}</div>
+            <div class="trash-task"><img src="svg/trash.svg" alt="trash" title="delete task"></div>
+            <div class="edit-task"><img src="svg/pencil.svg" alt="edit" title="edit task"></div>
         </section>
         
         `;
-        
+
+        sectionsArray.push(section);
         $('article').children().first().after(section);
     }
+
+    function saveTasks(){
+        localStorage.setItem('tasks', JSON.stringify(sectionsArray));
+
+    }
+
+    function loadTasks(){
+        var tasks = localStorage.getItem('tasks');
+        if (tasks) {
+            sectionsArray = JSON.parse(tasks);
+            sectionsArray.reverse();
+            $('article').children().first().after(sectionsArray.join(''));
+        }
+    }
+
+
+    $('article').on('click', '.trash-task', function() {
+        var section = $(this).closest('.task-section');
+        var index = $('.task-section').index(section);
+
+        // Remove a seção do array
+        sectionsArray.splice(index, 1);
+
+        // Atualiza o conteúdo do elemento article
+        $('article').html(sectionsArray.reverse().join(''));
+
+        // Salva as tarefas atualizadas
+        saveTasks();
+    });
     
 })
